a04aabf46134caa8a88e6d7e2292c44e0dae9f5b
Juaanx7 <juaanx7@gmail.com>
2024-10-03 15:42:20 -0300

    Creacion del proyecto con NavBar incluida

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>Mi ecommerce</h3>
+            <div>
+                <button>Categoria 1</button>
+                <button>Categoria 2</button>
+                <button>Categoria 3</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
