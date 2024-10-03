62f8de58e8bcd481d8a7c100d05c3c979f02eacf
Juaanx7 <juaanx7@gmail.com>
2024-10-03 16:44:15 -0300

    ultimos cambios en los estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import NavBar from './components/NavBar/NavBar.jsx'
-import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { useState } from "react";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar.jsx";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (    
+  return (
     <div className="App">
       <NavBar />
-      <ItemListContainer greeting={'¡Bienvenido!'}/>
+      <ItemListContainer greeting={"¡Bienvenido!"} />
     </div>
-      
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,27 +1,36 @@
-import CartWidget from '../CartWidget/CartWidget.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import CartWidget from "../CartWidget/CartWidget.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const NavBar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <h3 className="navbar-brand" href="#">Mi ecommerce</h3>
-            <div className="collapse navbar-collapse" id="navbarNav">
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <h3 className="navbar-brand" href="#">
+        Istorecba
+        </h3>
+        <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Categoria 1</a>
+              <a className="nav-link active" aria-current="page" href="#">
+                Iphone
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Categoria 2</a>
+              <a className="nav-link" href="#">
+                MacBook
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Categoria 3</a>
+              <a className="nav-link" href="#">
+                Accesorios
+              </a>
             </li>
           </ul>
           <CartWidget />
         </div>
-            
-        </nav>
-    )
-}
+      </div>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
